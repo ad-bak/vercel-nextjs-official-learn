2fe4c5f561663caa319895359791d25cdce2ec81
Adnan <adbak.dev@gmail.com>
2024-10-12 19:35:53 +0200

    chapter 7

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,5 +1,6 @@
 import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
+import { Fragment } from "react";
 
 export default function InvoiceStatus({ status }: { status: string }) {
 	return (
@@ -13,16 +14,16 @@ export default function InvoiceStatus({ status }: { status: string }) {
 			)}
 		>
 			{status === "pending" ? (
-				<>
+				<Fragment>
 					Pending
 					<ClockIcon className="ml-1 w-4 text-gray-500" />
-				</>
+				</Fragment>
 			) : null}
 			{status === "paid" ? (
-				<>
+				<Fragment>
 					Paid
 					<CheckIcon className="ml-1 w-4 text-white" />
-				</>
+				</Fragment>
 			) : null}
 		</span>
 	);
